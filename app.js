0473868c36a91265b2961966707d79d2cb503cd2
Your Name <morosanuiustin@gmail.com>
2020-02-13 19:27:36 +0000

    This is very nice hover image animation

---
+++ app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent:document.querySelector('.distorsion'),
+    intensity:0.5,
+    image1: './bw1.jpeg',
+    image2:'./bw2.jpeg',
+    angle: Math.PI /8,
+    speedIn: 2,
+    speedout:5,
+    displacementImage:'diss.png'
+});
